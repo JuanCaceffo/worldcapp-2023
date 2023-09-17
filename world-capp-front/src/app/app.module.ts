7b5418d5ffa54763a7f80b4699fe49be6b352f40
JuanCaceffo <caceffojuan@gmail.com>
2023-09-17 20:08:06 -0300

    comienzo de creacion de componente header en angular

--- world-capp-front/src/app/app.module.ts
+++ world-capp-front/src/app/app.module.ts
@@ -3,20 +3,21 @@ import { BrowserModule } from '@angular/platform-browser'
 
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
-import { ButtonComponent } from './button/button.component';
+import { ButtonComponent } from './button/button.component'
 import { InputComponent } from './input/input.component'
+import { HeaderComponent } from './header/header.component'
+import { NavbarComponent } from './navbar/navbar.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     ButtonComponent,
-    InputComponent
+    InputComponent,
+    HeaderComponent,
+    NavbarComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- world-capp-front/src/app/button/button.component.ts
+++ world-capp-front/src/app/button/button.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core'
 @Component({
   selector: 'app-button',
   templateUrl: './button.component.html',
-  styleUrls: ['../app.component.css','./button.component.css']
+  styleUrls: ['./button.component.css']
 })
 export class ButtonComponent {
 
