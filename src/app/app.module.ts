20f8f108dc4f308e5c033b258d8325c931292298
lopez-sol <lopez.msol.07@gmail.com>
2023-09-20 15:26:38 -0300

    Comienzo del componente footer en Angular y agregado del componente Logo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,11 @@ import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
 import { FormsModule } from '@angular/forms'
 import { FilterComponent } from './components/filter/filter.component'
+import { FooterComponent } from './components/footer/footer.component'
+import { LogoComponent } from './components/logo/logo.component'
 
 @NgModule({
-  declarations: [AppComponent, FilterComponent],
+  declarations: [AppComponent, FilterComponent, FooterComponent, LogoComponent],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
