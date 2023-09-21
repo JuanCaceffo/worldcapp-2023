19482751669ddc7f525df58b193d7214c0c50af2
JuanCaceffo <caceffojuan@gmail.com>
2023-09-21 13:02:16 -0300

    cambios en header eleminicacion de componente navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,15 +6,9 @@ import { AppComponent } from './app.component'
 import { HeaderComponent } from './components/header/header.component'
 import { FormsModule } from '@angular/forms'
 import { FilterComponent } from './components/filter/filter.component'
-import { NavBarComponent } from './components/header/nav-bar/nav-bar.component'
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    FilterComponent,
-    HeaderComponent,
-    NavBarComponent
-  ],
+  declarations: [AppComponent, FilterComponent, HeaderComponent],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
