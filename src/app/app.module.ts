13f8d1c505f39310ec3b455fbec9f159a6d05d10
madescoces <madescoces@gmail.com>
2023-09-20 21:46:20 -0300

    creación de componentes para cada vista

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,24 @@ import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
 import { FormsModule } from '@angular/forms'
 import { FilterComponent } from './components/filter/filter.component'
+import { LoginComponent } from './pages/login/login.component'
+import { CardSearchComponent } from './pages/card-search/card-search.component'
+import { CardAddComponent } from './pages/card-add/card-add.component'
+import { StoreSearchComponent } from './pages/store-search/store-search.component'
+import { CardDetailsComponent } from './pages/card-details/card-details.component'
+import { UserProfileComponent } from './pages/user-profile/user-profile.component'
 
 @NgModule({
-  declarations: [AppComponent, FilterComponent],
+  declarations: [
+    AppComponent,
+    FilterComponent,
+    LoginComponent,
+    CardSearchComponent,
+    CardAddComponent,
+    StoreSearchComponent,
+    CardDetailsComponent,
+    UserProfileComponent
+  ],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
