6ca8ee0bf468613ade447aa829e1dbf1c603d8e1
Facundo Barneche <fh.barneche@gmail.com>
2023-09-23 23:44:57 -0300

    binding entre vista y modelo

--- src/app/models/filter/sortedByFilter/sortedByFilter.model.ts
+++ src/app/models/filter/sortedByFilter/sortedByFilter.model.ts
@@ -2,6 +2,11 @@ import { IBaseFilter } from "../baseFilter.model"
 
 export class SortedByFilter implements IBaseFilter{
     radioOption = "closest"
-  
+
+    getFilterValue(event: Event){
+      const targetElement = (event.target as HTMLInputElement)
+      this.radioOption = targetElement.value
+    }
+
     getFigure = ():void => console.log(`Hola desde SortedByFilter`)
   }
