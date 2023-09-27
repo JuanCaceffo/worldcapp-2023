eaaec928798cca99d398268392bb0cc683d0a660
madescoces <madescoces@gmail.com>
2023-09-27 14:42:05 -0300

    Avances en implementación de strategy para input

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,45 +1,26 @@
 import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
 
-// import { AppRoutingModule, routingComponents } from './app-routing.module'
+import { AppRoutingModule, routingComponents } from './app-routing.module'
 import { AppComponent } from './app.component'
-import { HeaderComponent } from './components/header/header.component'
 import { FormsModule } from '@angular/forms'
-
-import { BaseFilterComponent } from './components/filter/baseFilter.component'
-import { SortedByFilterComponent } from './components/filter/sortedByFilter/sortedByFilter.component'
-import { VariantFilterComponent } from './components/filter/variantFilter/variantFilter.component'
 import { FooterComponent } from './components/footer/footer.component'
 import { LogoComponent } from './components/logo/logo.component'
-import { CardDetailsComponent } from './pages/card-details/card-details.component'
-import { UserProfileComponent } from './pages/user-profile/user-profile.component'
-import { LoginComponent } from './pages/login/login.component'
-import { CardSearchComponent } from './pages/card-search/card-search.component'
-import { StoreSearchComponent } from './pages/store-search/store-search.component'
-import { CardAddComponent } from './pages/card-add/card-add.component'
 import { CFieldComponent } from './components/c-field/c-field.component'
+import { HeaderComponent } from './components/header/header.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
     LogoComponent,
-    // routingComponents,
-    BaseFilterComponent,
-    SortedByFilterComponent,
-    VariantFilterComponent,
-    HeaderComponent,
-    LoginComponent,
-    CardSearchComponent,
-    CardDetailsComponent,
-    StoreSearchComponent,
-    UserProfileComponent,
-    CardAddComponent,
-    CFieldComponent
+    routingComponents,
+    CFieldComponent,
+    HeaderComponent
     // FormsModule,
     // BrowserModule
   ],
-  imports: [BrowserModule, /*AppRoutingModule,*/ FormsModule],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/app/components/c-field/c-field.component.ts
+++ src/app/components/c-field/c-field.component.ts
@@ -1,17 +1,56 @@
-import { Component, HostBinding, Input } from '@angular/core'
-import { CustomFieldProps } from 'src/app/models/c-field/c-field.model';
+import { Component } from '@angular/core'
+
+export interface FieldProps {
+  class: string
+  label: string  
+  value: string
+  default?: string
+  placeholder?: string
+  options?: string[]
+}
 
 @Component({
-  selector: 'app-c-field',
+  templateUrl: '/c-input/c-input.html',
+  styleUrls: ['./c-input/c-input.css']
+})
+class CustomInput{
+
+}
+
+@Component({
+  templateUrl: './c-select/c-select.html',
+  styleUrls: ['./c-select/c-select.css']
+})
+class CustomSelect {
+  
+}
+
+@Component({
+  selector: 'app-field',
   templateUrl: './c-field.component.html',
   styleUrls: ['./c-field.component.css']
 })
-export class CFieldComponent {
-  @HostBinding('class') className = 'app-input'
-  @Input() fieldProperties: CustomFieldProps;
+export class CustomField implements FieldProps {
+  class = ''
+  label = ''
+  value!: string
+  default = ''
+  placeholder = ''
+  options = []
 
-  // Determinar inputType automáticamente
-  ngOnInit() {
-    @Input() inputType = this.fieldProperties.selectOptions ? 'select' : 'text';
+  behavior: CustomInput | CustomSelect
+  
+  constructor() {
+    // Initialize behavior based on the presence of options
+    this.behavior = this.options && this.options.length > 0 ? new CustomSelect() : new CustomInput()
   }
+
+  ngOnInit(){
+    
+  }
+
+
 }
+
+
+
