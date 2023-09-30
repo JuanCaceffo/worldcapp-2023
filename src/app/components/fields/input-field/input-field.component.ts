38f51bf104c6f6cfa0a82dceee18fbad70341977
madescoces <madescoces@gmail.com>
2023-09-29 22:30:40 -0300

    Error msg y estilos activo

--- src/app/components/fields/input-field/input-field.component.ts
+++ src/app/components/fields/input-field/input-field.component.ts
@@ -2,6 +2,7 @@ import { FieldComponent } from 'src/app/components/fields/field.component'
 import { InputFieldProps } from 'src/app/interfaces/field'
 import { Component, Input, OnInit} from '@angular/core'
 
+const MAXERRORLENGTH = 40
 @Component({
   selector: 'app-input-field',
   templateUrl: './input-field.component.html',
@@ -13,20 +14,38 @@ export class InputFieldComponent
 {
   @Input() placeholder = 'Escriba aquí'
   @Input() type = 'text'
-  @Input() error: string | undefined
-     
+  @Input() error = ''  
+  
   constructor() {
     super()
   }
-
+  
   ngOnInit(): void {
     // console.log(this.label)
     // console.log(this.className)        
   }
 
-
   override cssVariant(): string {
     return ''
+  }
+
+  override cssClass(): string {
+    return super.cssClass() + this.errorStyle()
+  }
+
+  errorMessage(): string {
+    this.validateErrorMessage()
+    return this.error
+  }
+
+  errorStyle(): string {
+    return this.error !== '' ? 'field--error' : ''
+  }
+
+  validateErrorMessage(){    
+    if(this.error.length > MAXERRORLENGTH) {
+      throw new Error(`Too long error msg, ${this.error.length - MAXERRORLENGTH} chars exeeded of ${MAXERRORLENGTH}.`)
+    }
   }
 
   // isNumeric(): boolean {
