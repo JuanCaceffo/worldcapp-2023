a323ab8d4d6df306d619b7a5c0fbe271b11b1ff6
JuanCaceffo <caceffojuan@gmail.com>
2023-10-01 17:35:43 -0300

    inyeccion de service de cards y agregado de cards a la estructura

--- src/app/pages/card-search/card-search.component.ts
+++ src/app/pages/card-search/card-search.component.ts
@@ -1,14 +1,22 @@
-import { Component } from '@angular/core'
+import { CardService } from './../../data/services/cardService/Card.service'
+import { Component, OnInit } from '@angular/core'
 import { Title } from '@angular/platform-browser'
+import { Card } from 'src/app/data/models/cards/Card'
 
 @Component({
   selector: 'app-card-search',
   templateUrl: './card-search.component.html',
   styleUrls: ['./card-search.component.css']
 })
-export class CardSearchComponent {
-  constructor(private titleService: Title) {
+export class CardSearchComponent implements OnInit {
+  ngOnInit() {
     this.titleService.setTitle('Figuritas')
+    this.listCards = this.getAllCards()
+  }
+  constructor(private titleService: Title, public cardService: CardService) {}
+  listCards: Array<Card> = []
+
+  getAllCards() {
+    return this.cardService.getAllCards()
   }
 }
-
