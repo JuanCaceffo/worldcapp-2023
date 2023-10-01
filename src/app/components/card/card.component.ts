64ac73e744028aa06333f1e8d0aff2a9f2aaddab
JuanCaceffo <caceffojuan@gmail.com>
2023-10-01 17:33:43 -0300

    adaptacion a los cambios del modelo de domino card

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -8,8 +8,6 @@ import { Card } from 'src/app/data/models/cards/Card'
 })
 export class CardComponent implements OnInit {
   @Input() card!: Card
-  //TODO: probar con injeccion de dependencias por meido de constructor
-  props = this.card.cardProps
 
   ngOnInit() {}
 }
