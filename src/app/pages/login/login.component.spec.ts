29984222d4d537e1bbd01c90e24dc0d33f86df74
lopez-sol <lopez.msol.07@gmail.com>
2023-10-06 16:23:40 -0300

    Agregado de tests (incompleto con errores)

--- src/app/pages/login/login.component.spec.ts
+++ src/app/pages/login/login.component.spec.ts
@@ -1,50 +1,56 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing'
+import { IconFieldComponent } from './../../components/fields/icon-field/icon-field.component'
+import { ComponentFixture, TestBed, fakeAsync } from '@angular/core/testing'
 import { LoginComponent } from './login.component'
 import { LogoComponent } from 'src/app/components/logo/logo.component'
-import { FieldComponent } from 'src/app/components/fields/field.component'
-import { IconFieldComponent } from 'src/app/components/fields/icon-field/icon-field.component'
 import { InputFieldComponent } from 'src/app/components/fields/input-field/input-field.component'
 import { FieldLabelComponent } from 'src/app/components/fields/field-label/field-label.component'
+import { FieldComponent } from 'src/app/components/fields/field.component'
 
 describe('LoginComponent', () => {
   let component: LoginComponent
   let fixture: ComponentFixture<LoginComponent>
 
-  beforeEach(() => {
+  beforeEach(fakeAsync(() => {
     TestBed.configureTestingModule({
-      declarations: [LoginComponent, LogoComponent, FieldComponent, IconFieldComponent, InputFieldComponent, FieldLabelComponent]
+      declarations: [LoginComponent, LogoComponent, IconFieldComponent, InputFieldComponent, FieldLabelComponent, FieldComponent]
     })
     fixture = TestBed.createComponent(LoginComponent)
     component = fixture.componentInstance
     fixture.detectChanges()
-  })
+  }))
+
+  const getByTestId = (testId: string) => {
+    return fixture.debugElement.nativeElement.querySelector(
+      `[data-testid="${testId}"]`
+    ) as HTMLElement
+  }
 
   it('should create', () => {
     expect(component).toBeTruthy()
   })
 
-  it('los campos de usuario y contraseña deben estar en pantalla', () => {
-    const usernameInput = fixture.nativeElement.querySelector('#user')
-    const passwordInput = fixture.nativeElement.querySelector('#password')
+  // it('los campos de usuario y contraseña deben estar en pantalla', () => {
+    // const usernameInput: HTMLElement = getByTestId('userInput')
+    // const passwordInput: HTMLElement = getByTestId('passwordInput')
   
-    expect(usernameInput).toBeTruthy()
-    expect(passwordInput).toBeTruthy()
-  })
+    // expect(usernameInput).toBeTruthy()
+    // expect(passwordInput).toBeTruthy()
+  // })
   
-  it('should update username and password properties', () => {
-    const usernameInput = fixture.nativeElement.querySelector('#username')
-    const passwordInput = fixture.nativeElement.querySelector('#password')
+  // it('should update username and password properties', () => {
+    // const usernameInput: HTMLElement = getByTestId('userInput')
+    // const passwordInput: HTMLElement = getByTestId('passwordInput')
   
-    usernameInput.value = 'sol'
-    passwordInput.value = '1234'
+    // usernameInput.value = 'sol'
+    // passwordInput.value = '1234'
   
-    usernameInput.dispatchEvent(new Event('input'))
-    passwordInput.dispatchEvent(new Event('input'))
+    // usernameInput.dispatchEvent(new Event('input'))
+    // passwordInput.dispatchEvent(new Event('input'))
   
-    fixture.detectChanges()
+    // fixture.detectChanges()
   
-    expect(component.loginData.user).toBe('sol')
-    expect(component.loginData.password).toBe('1234')
-  })
+    // expect(component.loginData.user).toBe('sol')
+    // expect(component.loginData.password).toBe('1234')
+  // })
   
 })
