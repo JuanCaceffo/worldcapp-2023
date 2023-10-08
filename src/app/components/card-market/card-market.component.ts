51bbeb06509e9df59306f72fb35c745d1cb54482
lopez-sol <lopez.msol.07@gmail.com>
2023-10-08 20:11:14 -0300

    Arreglo de la visual de los markets en pantalla / Agregado de Service funcional para los markets

--- src/app/components/card-market/card-market.component.ts
+++ src/app/components/card-market/card-market.component.ts
@@ -1,5 +1,7 @@
+import { CardMarketService } from './../../services/card-market.service'
 import { Component, OnInit } from '@angular/core'
-import { PickupPoint } from 'src/app/models/pickupPoint/pickupPoint'
+import { IPickupPoint } from 'src/app/interfaces/pickupPoint'
+import { PickupPoint, PickupPoint2 } from 'src/app/models/pickupPoint/pickupPoint'
 
 @Component({
   selector: 'app-card-market',
@@ -7,17 +9,26 @@ import { PickupPoint } from 'src/app/models/pickupPoint/pickupPoint'
   styleUrls: ['./card-market.component.css']
 })
 export class CardMarketComponent implements OnInit {
-  marketCards!: PickupPoint[]
+  marketCards!: IPickupPoint[]
 
+  constructor(public cardMarketService: CardMarketService) { }
   //TODO:Puntos de ventas devuelve o kiosko o lirebria, etc. Ver nombre de clase y respecto a eso
   //Automatizar el iconito en la vista
 
+  getType(data: IPickupPoint): PickupPoint | PickupPoint2 {
+    return data
+  }
+
   ngOnInit() {
     //TODO: Sacar Kiosko de aca y ver en el back si es un tipo
-    this.marketCards = [
-      new PickupPoint('Kiosko La Scaloneta', 'Alpatacal 1234', 1, 3, 4),
-      new PickupPoint('Kiosko LaLALA', 'Alpatacal 1234', 1, 3, 4),
-      new PickupPoint('Kiosko La CACHO CACHEFFO,a', 'Alpatacal 1234', 1, 3, 4)
-    ]
+    // this.marketCards = [
+    //   new PickupPoint('Kiosko La Scaloneta', 'Alpatacal 1234', 1, 3, 4),
+    //   new PickupPoint('Kiosko LaLALA', 'Alpatacal 1234', 1, 3, 4),
+    //   new PickupPoint('Kiosko La CACHO CACHEFFO,a', 'Alpatacal 1234', 1, 3, 4),
+    //   new PickupPoint('Kiosko La Scaloneta', 'Alpatacal 1234', 1, 3, 4),
+    //   new PickupPoint('Kiosko LaLALA', 'Alpatacal 1234', 1, 3, 4),
+    //   new PickupPoint('Kiosko La CACHO CACHEFFO,a', 'Alpatacal 1234', 1, 3, 4)
+    // ]
+    this.marketCards = this.cardMarketService.buscarMarket()
   }
 }
--- src/app/services/card-market.service.ts
+++ src/app/services/card-market.service.ts
@@ -1,8 +1,25 @@
 import { Injectable } from '@angular/core'
+import { PickupPoint, PickupPoint2 } from '../models/pickupPoint/pickupPoint'
+import { IPickupPoint } from '../interfaces/pickupPoint'
 
 @Injectable({
   providedIn: 'root'
 })
 export class CardMarketService {
-  constructor() {}
+  marketCards!: IPickupPoint[]
+
+  constructor() {
+    this.marketCards = [
+      new PickupPoint('Kiosko La Scaloneta', 'Alpatacal 1234', 1, 3, 4),
+      new PickupPoint('Kiosko LaLALA', 'Alpatacal 1234', 1, 3, 4),
+      new PickupPoint('Kiosko La CACHO CACHEFFO,a', 'Alpatacal 1234', 1, 3, 4),
+      new PickupPoint2('Kiosko La Scaloneta', 'Alpatacal 1234', 1, 3, 4),
+      new PickupPoint('Kiosko LaLALA', 'Alpatacal 1234', 1, 3, 4),
+      new PickupPoint('Kiosko La CACHO CACHEFFO,a', 'Alpatacal 1234', 1, 3, 4)
+    ]
+  }
+
+  buscarMarket(): IPickupPoint[]{
+    return this.marketCards
+  }
 }
