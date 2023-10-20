93ad4903a9d7c247e96766cef2d7e6d116896208
JuanCaceffo <caceffojuan@gmail.com>
2023-10-20 16:46:11 -0300

    agregado de routing anidado en la pagina perfil de usuario y eliminado de info perfil de app.module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,20 +1,31 @@
-import { CardDetailsComponent } from './pages/card-details/card-details.component'
-import { UserProfileComponent } from './pages/user-profile/user-profile.component'
-import { NgModule } from '@angular/core'
-import { RouterModule, Routes } from '@angular/router'
-import { CardAddComponent } from './pages/card-add/card-add.component'
-import { LoginComponent } from './pages/login/login.component'
-import { CardSearchComponent } from './pages/card-search/card-search.component'
-import { StoreSearchComponent } from './pages/store-search/store-search.component'
+import {CardDetailsComponent} from './pages/card-details/card-details.component'
+import {UserProfileComponent} from './pages/user-profile/user-profile.component'
+import {NgModule} from '@angular/core'
+import {RouterModule, Routes} from '@angular/router'
+import {CardAddComponent} from './pages/card-add/card-add.component'
+import {LoginComponent} from './pages/login/login.component'
+import {CardSearchComponent} from './pages/card-search/card-search.component'
+import {StoreSearchComponent} from './pages/store-search/store-search.component'
+import {ProfileMissingFigusComponent} from './components/profile-missing-figus/profile-missing-figus.component'
+import {ProfileDuplicateFigusComponent} from './components/profile-duplicate-figus/profile-duplicate-figus.component'
+import {ProfileInfoComponent} from './components/profile-info/profile-info.component'
 
 export const routes: Routes = [
-  { path: 'login', component: LoginComponent },
-  { path: 'figuritas', component: CardSearchComponent },
-  { path: 'detalle-figurita/:id', component: CardDetailsComponent },
-  { path: 'sobres', component: StoreSearchComponent },
-  { path: 'perfil', component: UserProfileComponent },
-  { path: 'agregar-figuritas', component: CardAddComponent },
-  { path: '**', redirectTo: 'login', pathMatch: 'full' }
+  {path: 'login', component: LoginComponent},
+  {path: 'figuritas', component: CardSearchComponent},
+  {path: 'detalle-figurita/:id', component: CardDetailsComponent},
+  {path: 'sobres', component: StoreSearchComponent},
+  {
+    path: 'perfil',
+    component: UserProfileComponent,
+    children: [
+      {path: 'perfil-usuario', component: ProfileInfoComponent},
+      {path: 'figuritas-repetidas', component: ProfileDuplicateFigusComponent},
+      {path: 'figuritas-faltantes', component: ProfileMissingFigusComponent}
+    ]
+  },
+  {path: 'agregar-figuritas', component: CardAddComponent},
+  {path: '**', redirectTo: 'login', pathMatch: 'full'}
 ]
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
