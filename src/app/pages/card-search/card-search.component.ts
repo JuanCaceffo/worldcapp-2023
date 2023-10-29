0b989650348dd555503da25e4b8f5d0f3ff41d12
JuanCaceffo <caceffojuan@gmail.com>
2023-10-29 17:27:34 -0300

    eliminado de import en desuso

--- src/app/pages/card-search/card-search.component.ts
+++ src/app/pages/card-search/card-search.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit} from '@angular/core'
+import {Component, OnInit} from '@angular/core'
 import {Title} from '@angular/platform-browser'
 import {Figurita} from 'src/app/models/cards/figurita.model'
 import {CardSearch} from 'src/app/models/searchbar/searchbar'
