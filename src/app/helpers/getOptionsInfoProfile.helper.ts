b6ba63db3ca25354378d035df2a1abec3b272d90
Facundo Barneche <fh.barneche@gmail.com>
2023-10-29 17:24:38 -0300

    improvement:Se borra mock sin usar

--- src/app/helpers/getOptionsInfoProfile.helper.ts
+++ src/app/helpers/getOptionsInfoProfile.helper.ts
@@ -1,31 +1,3 @@
-import {ProvinceDTO} from '../dtos/province.dto'
-
-export const provincesMocked: ProvinceDTO[] = [
-  {
-    province: 'Buenos Aires',
-    locations: [
-      'Avellaneda',
-      'Bahía Blanca',
-      'La Plata',
-      'Mar del Plata',
-      'San Isidro',
-      'Tandil',
-      'Vicente López'
-    ]
-  },
-  {
-    province: 'CABA',
-    locations: [
-      'Balvanera',
-      'Belgrano',
-      'Palermo',
-      'San Nicolás',
-      'San Telmo',
-      'Villa Crespo'
-    ]
-  }
-]
-
 export const criteria: string[] = [
   'Desprendido',
   'Par',
@@ -35,29 +7,4 @@ export const criteria: string[] = [
   'Interesado',
   'Cambiante',
   'Fanático'
-]
-
-export const locations: string[] = [
-  'Avellaneda',
-  'Azul',
-  'Bahía Blanca',
-  'Berazategui',
-  'Junín',
-  'La Plata',
-  'Lomas de Zamora',
-  'Luján',
-  'Mar del Plata',
-  'Merlo',
-  'Morón',
-  'Olavarría',
-  'Pergamino',
-  'Quilmes',
-  'San Isidro',
-  'San Martín',
-  'San Miguel',
-  'San Nicolás',
-  'San Salvador de Jujuy',
-  'Tandil',
-  'Tigre',
-  '3 de Febrero'
 ]
