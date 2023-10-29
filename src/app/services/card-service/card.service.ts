20f8c0373dd6cc81799cbdd6b756f7d9aaf88766
madescoces <madescoces@gmail.com>
2023-10-29 06:54:43 -0300

    Refactor filtros y lógica del cardservice

--- src/app/services/card-service/card.service.ts
+++ src/app/services/card-service/card.service.ts
@@ -5,9 +5,7 @@ import {FiguritaDTO} from 'src/app/dtos/figurita.dto'
 import {API_URL} from '../config'
 import {lastValueFrom} from 'rxjs'
 import {getUserId} from 'src/app/helpers/getUserId.helper'
-import {HttpParams} from '@angular/common/http'
 import { CardSearch } from 'src/app/models/searchbar/searchbar'
-import { CardSearchProps } from 'src/app/interfaces/searchCriteria'
 
 @Injectable({
   providedIn: 'root'
@@ -16,20 +14,10 @@ export class CardService {
 
   constructor(private httpClient: HttpClient, public cardSearch: CardSearch) {}
 
-  async getCards(): Promise<Figurita[]> {        
-    const figuritas = this.httpClient.get<FiguritaDTO[]>(
-      `${API_URL}/figuritas/intercambiar/${getUserId()}`, {params: this.mappingHTTP()}
-    )
-    const figuritasJSON = await lastValueFrom(figuritas)
-    // console.log(figuritasJSON)
+  async getCards(): Promise<Figurita[]> {               
+    const figuritasJSON = await lastValueFrom(this.cardData())    
     return figuritasJSON.map((card) => Figurita.fromJson(card))
   }
-
-  mappingHTTP(): HttpParams{
-    let httpParams = new HttpParams()
-    Object.keys(this.cardSearch).forEach( key => {            
-      httpParams = httpParams.set(key, this.cardSearch[key as keyof CardSearchProps].toString())            
-    })    
-    return httpParams
-  }
+  
+  private cardData = () => this.httpClient.get<FiguritaDTO[]>(`${API_URL}/figuritas/intercambiar/${getUserId()}`, {params: this.cardSearch.httpParams()})
 }
