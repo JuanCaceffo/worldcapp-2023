519823c7a361dde5905c5d9a80a4b407c7542b4c
JuanCaceffo <caceffojuan@gmail.com>
2023-10-31 12:11:48 -0300

    Solucion capturado de errores del back en error handler

--- src/app/helpers/errorHandler.ts
+++ src/app/helpers/errorHandler.ts
@@ -1,21 +1,19 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 export function mostrarError(component: any, error: any): void {
-  console.log(error)
+  const errorResponse = error.error
+
   let errorMessage = ''
   if (typeof error === 'string') {
     errorMessage = error
-  } else {
-    errorMessage = error.message ?? ''
-  }
-  if (error.status === 0) {
+  } else if (!errorResponse.status) {
     errorMessage =
       'No hay conexión con el backend, revise si el servidor remoto está levantado.'
-  } else if (error.status >= 500) {
+  } else if (errorResponse.status >= 500) {
     errorMessage =
       'Hubo un error al realizar la operación. Consulte al administrador del sistema.'
-  } else if (error.status >= 400) {
+  } else if (errorResponse.status >= 400) {
     errorMessage =
-      errorMessage ||
+      errorResponse.message ||
       'Se ha producido un error en la solicitud. Por favor, verifique los datos proporcionados y vuelva a intentarlo.'
   }
   component.errors.push(errorMessage)
