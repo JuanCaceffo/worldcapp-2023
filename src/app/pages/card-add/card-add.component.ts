2eedc291834c05a6af7aa1269dc656dcdb2bcbb9
JuanCaceffo <caceffojuan@gmail.com>
2023-10-31 13:21:58 -0300

    eliminado de pasaje de tipo de figurita por parametro al navegar a pantalla de agregado de figus

--- src/app/pages/card-add/card-add.component.ts
+++ src/app/pages/card-add/card-add.component.ts
@@ -1,7 +1,6 @@
 import {Component} from '@angular/core'
 import {Title} from '@angular/platform-browser'
 import {ActivatedRoute} from '@angular/router'
-import {FigusListType} from 'src/app/dtos/figurita.dto'
 
 @Component({
   selector: 'app-card-add',
@@ -13,10 +12,6 @@ export class CardAddComponent {
 
   ngOnInit() {
     this.titleService.setTitle('Agregar Figuritas')
-    this.route.params.subscribe((param) => {
-      this.figusType = param['figus-list-type'] as FigusListType
-      console.log(this.figusType)
-    })
   }
   figusType!: string
 }
