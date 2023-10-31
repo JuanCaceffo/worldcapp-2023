f8be16d4189826ab21e8d14809176cde6b62d2ba
JuanCaceffo <caceffojuan@gmail.com>
2023-10-31 16:51:13 -0300

    agregado metodo para obtener figuritas agregables

--- src/app/services/card-service/card.service.ts
+++ src/app/services/card-service/card.service.ts
@@ -5,7 +5,7 @@ import {FiguritaDTO} from 'src/app/dtos/figurita.dto'
 import {API_URL} from '../config'
 import {lastValueFrom} from 'rxjs'
 import {getUserId} from 'src/app/helpers/getUserId.helper'
-import { cardFilter } from 'src/app/models/searchbar/searchbar'
+import {cardFilter} from 'src/app/models/searchbar/searchbar'
 
 @Injectable({
   providedIn: 'root'
@@ -14,10 +14,20 @@ export class CardService {
   constructor(private httpClient: HttpClient) {}
   filter = cardFilter
 
-  async getCards(): Promise<Figurita[]> {               
-    const figuritasJSON = await lastValueFrom(this.cardData())    
+  async getCards(): Promise<Figurita[]> {
+    const figuritasJSON = await lastValueFrom(
+      this.cardData(`${API_URL}/figuritas/intercambiar/${getUserId()}`)
+    )
     return figuritasJSON.map((card) => Figurita.fromJson(card))
   }
 
-  private cardData = () => this.httpClient.get<FiguritaDTO[]>(`${API_URL}/figuritas/intercambiar/${getUserId()}`, {params: this.filter.httpParams()})
+  async getCollectibleFigus(): Promise<Figurita[]> {
+    const figusJSON = await lastValueFrom(
+      this.cardData(`${API_URL}/figuritas/figus-agregables`)
+    )
+    return figusJSON.map((card) => Figurita.fromJson(card))
+  }
+
+  private cardData = (path: string) =>
+    this.httpClient.get<FiguritaDTO[]>(path, {params: this.filter.httpParams()})
 }
