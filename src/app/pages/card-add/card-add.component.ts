27c09ef4d51c953b9d48e7285f8d8a6f1afd984e
JuanCaceffo <caceffojuan@gmail.com>
2023-11-05 00:44:02 -0300

    adpatacion al routing data en card add

--- src/app/pages/card-add/card-add.component.ts
+++ src/app/pages/card-add/card-add.component.ts
@@ -1,12 +1,11 @@
 import {UserService} from 'src/app/services/user-service/user.service'
 import {CardService} from './../../services/card-service/card.service'
 import {Component} from '@angular/core'
-import {Title} from '@angular/platform-browser'
 import {Figurita} from 'src/app/models/cards/figurita.model'
 import {ActivatedRoute} from '@angular/router'
-import {FigusListType} from 'src/app/dtos/figurita.dto'
 import {mostrarError} from 'src/app/helpers/errorHandler'
-import { CardSearch } from 'src/app/models/searchbar/searchbar'
+import {CardSearch} from 'src/app/models/searchbar/searchbar'
+import {DataCardAddProfile} from 'src/app/app-routing.module'
 
 @Component({
   selector: 'app-card-add',
@@ -15,31 +14,32 @@ import { CardSearch } from 'src/app/models/searchbar/searchbar'
 })
 export class CardAddComponent {
   constructor(
-    private titleService: Title,
     private cardService: CardService,
     private userService: UserService,
     private route: ActivatedRoute
   ) {}
 
   ngOnInit() {
-    this.titleService.setTitle('Agregar Figuritas')
-    this.route.params.subscribe((param) => {
-      this.listCardType = param['figus-list-type'] as FigusListType
-      console.log(this.listCardType)
+    this.route.data.subscribe((data) => {
+      this.routeData = data as DataCardAddProfile
     })
   }
-  listCardType!: FigusListType
   listCards: Figurita[] = []
   errors: string[] = []
   filter = new CardSearch()
+  routeData!: DataCardAddProfile
 
-  async getAll() {
-    this.listCards = await this.cardService.getCollectibleFigus(this.filter)
+  getAll = async () => {
+    this.listCards = await this.routeData.getFigus(
+      this.cardService,
+      this.filter
+    )
   }
 
-  async addCard(card: Figurita) {
-    await this.userService.addFigurita(card.props.id, this.listCardType)
+  addCard = async (card: Figurita) => {
     try {
+      await this.routeData.onClickCard(card, this.userService)
+      await this.getAll()
       alert('figurita agregada con exito')
     } catch (error) {
       mostrarError(this, error)
