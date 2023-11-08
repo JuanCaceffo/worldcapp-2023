6f17bb9a4ca92f24404755954cb6a3ef36c163e6
alemeni2001 <alejomenini@gmail.com>
2023-11-08 18:04:53 -0300

    Se arreglaron problemas del test de navbar que antes estaba todo comentado y se agrego el test de la ruta correspondiente

--- src/app/components/navbar-profile/navbar-profile.component.spec.ts
+++ src/app/components/navbar-profile/navbar-profile.component.spec.ts
@@ -1,29 +1,25 @@
 /* tslint:disable:no-unused-variable */
-/*
 import {ComponentFixture, TestBed, fakeAsync} from '@angular/core/testing'
 import {NavbarProfileComponent} from './navbar-profile.component'
 import {RouterTestingModule} from '@angular/router/testing'
-import {Router} from '@angular/router'
+import {Location} from '@angular/common'
 import {By} from '@angular/platform-browser'
+import {AppRoutingModule} from 'src/app/app-routing.module'
 
-describe('NavbarProfileComponent', () => {
+describe('NavbarProfileComponent', async () => {
   let component: NavbarProfileComponent
   let fixture: ComponentFixture<NavbarProfileComponent>
-  let routerSpy: jasmine.SpyObj<Router>
+  let location: Location
 
   beforeEach(fakeAsync(() => {
-    routerSpy = jasmine.createSpyObj('Router', ['url'])
-
     TestBed.configureTestingModule({
-      imports: [RouterTestingModule],
-      declarations: [NavbarProfileComponent],
-      providers: [
-        {provide: Router, useValue: routerSpy} // Espía del Router
-      ]
+      imports: [RouterTestingModule, AppRoutingModule],
+      declarations: [NavbarProfileComponent]
     }).compileComponents()
   }))
 
   beforeEach(() => {
+    location = TestBed.inject(Location)
     fixture = TestBed.createComponent(NavbarProfileComponent)
     component = fixture.componentInstance
     fixture.detectChanges()
@@ -34,32 +30,18 @@ describe('NavbarProfileComponent', () => {
   })
 
   it('debería mostrar listProps correctamente', () => {
-    const userProfilePaths = {
-      info: 'perfil-usuario',
-      duplicateFigus: 'figuritas-repetidas',
-      missingFigus: 'figuritas-faltantes'
-    }
-    component.listProps = [
-      {
-        content: 'Información General',
-        url: `/perfil/${userProfilePaths.info}`
-      },
-      {
-        content: 'Figuritas Repetidas',
-        url: `/perfil/${userProfilePaths.duplicateFigus}`
-      },
-      {
-        content: 'Figuritas Faltantes',
-        url: `/perfil/${userProfilePaths.missingFigus}`
-      }
-    ]
-
-    fixture.detectChanges()
-
     const tabItems = fixture.debugElement.queryAll(
       By.css('[data-testid="tab-button"]')
     )
     expect(tabItems.length).toBe(3)
   })
+  it('cuando hago click en el tab, me deberia mandar a la ruta correspondiente', async () => {
+    const tabItems = fixture.debugElement.queryAll(
+      By.css('[data-testid="tab-button"]')
+    )
+    tabItems[0].nativeElement.click()
+    await fixture.whenStable()
+
+    expect(location.path()).toBe('/perfil/perfil-usuario')
+  })
 })
-*/
